9b8f99645d22d494c4fff1ae22e7fb2c22a0c128
tran ngoc tu <tutran985@gmail.com>
2019-07-12 09:22:21 +0700

    cors

--- ninegag/settings.py
+++ ninegag/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-cszl4oka%$6!(_k+7f_l!@_+h0c&po5n4^&tydoai%h9yz@iu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['27.72.88.246']
 
